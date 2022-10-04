d35723c6dbbccc61ee6c70497a4834e3b2c99acf
profescore <profescore2010@gmail.com>
2022-10-04 11:09:05 +0400

    Update Startup.cs

--- TailwindTraders.Website/Source/Tailwind.Traders.Web/Startup.cs
+++ TailwindTraders.Website/Source/Tailwind.Traders.Web/Startup.cs
@@ -7,7 +7,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Tailwind.Traders.Web.Standalone;
-
+//trigger Pr build
 namespace Tailwind.Traders.Web
 {
     public class Startup
